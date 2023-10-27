bff97036b43895eb5ab1312b7535fdaf1f08c57e
James Israel <yongeazi143@gmail.com>
2023-10-27 18:32:42 +0100

    added tsconfig

--- api/generate.ts
+++ api/generate.ts
@@ -1,54 +1,53 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { kv } from "@vercel/kv";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from "openai";
 import { Ratelimit } from "@upstash/ratelimit";
 
-// Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || "",
 });
 
+
 // IMPORTANT! Set the runtime to edge: https://vercel.com/docs/functions/edge-functions/edge-runtime
 export const runtime = "edge";
 
-export async function POST(req: Request): Promise<Response> {
-  // Check if the OPENAI_API_KEY is set, if not return 400
+
+export default async function handler(req: VercelRequest, res: VercelResponse): Promise<void> {
   if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
-    return new Response(
-      "Missing OPENAI_API_KEY – make sure to add it to your .env file.",
-      {
-        status: 400,
-      },
-    );
+    res.status(400).json({ error: "Missing OPENAI_API_KEY – make sure to add it to your environment variables." });
+    return;
   }
+
   if (
-    process.env.NODE_ENV != "development" &&
+    process.env.NODE_ENV !== "development" &&
     process.env.KV_REST_API_URL &&
     process.env.KV_REST_API_TOKEN
   ) {
-    const ip = req.headers.get("x-forwarded-for");
+    const ip = req.headers["x-forwarded-for"];
     const ratelimit = new Ratelimit({
       redis: kv,
       limiter: Ratelimit.slidingWindow(50, "1 d"),
     });
 
     const { success, limit, reset, remaining } = await ratelimit.limit(
-      `novel_ratelimit_${ip}`,
+      `novel_ratelimit_${ip}`
     );
 
     if (!success) {
-      return new Response("You have reached your request limit for the day.", {
-        status: 429,
-        headers: {
+      res
+        .status(429)
+        .set({
           "X-RateLimit-Limit": limit.toString(),
           "X-RateLimit-Remaining": remaining.toString(),
           "X-RateLimit-Reset": reset.toString(),
-        },
-      });
+        })
+        .json({ error: "You have reached your request limit for the day." });
+      return;
     }
   }
 
-  let { prompt } = await req.json();
+  const { prompt } = req.body;
 
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
@@ -59,8 +58,6 @@ export async function POST(req: Request): Promise<Response> {
           "You are an AI writing assistant that continues existing text based on context from prior text. " +
           "Give more weight/priority to the later characters than the beginning ones. " +
           "Limit your response to no more than 200 characters, but make sure to construct complete sentences.",
-        // we're disabling markdown for now until we can figure out a way to stream markdown text with proper formatting: https://github.com/steven-tey/novel/discussions/7
-        // "Use Markdown formatting when appropriate.",
       },
       {
         role: "user",
@@ -75,9 +72,12 @@ export async function POST(req: Request): Promise<Response> {
     n: 1,
   });
 
+  res.status(200).json(response);
+  
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
 
   // Respond with the stream
   return new StreamingTextResponse(stream);
+
 }
