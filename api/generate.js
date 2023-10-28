8ad1d798fc5dee5b15b205ab2b24c3d744f4ac65
James Israel <yongeazi143@gmail.com>
2023-10-28 02:10:36 +0100

    updateding endpoint

--- api/generate.js
+++ api/generate.js
@@ -57,13 +57,14 @@ export async function POST(req) {
         content:
           "You are an AI writing assistant that continues existing text based on context from prior text. " +
           "Give more weight/priority to the later characters than the beginning ones. " +
-          "Limit your response to no more than 200 characters, but make sure to construct complete sentences.",
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
+          "",
         // we're disabling markdown for now until we can figure out a way to stream markdown text with proper formatting: https://github.com/steven-tey/novel/discussions/7
         // "Use Markdown formatting when appropriate.",
       },
       {
         role: "user",
-        content: prompt,
+        content: "" + prompt,
       },
     ],
     temperature: 0.7,
@@ -75,7 +76,7 @@ export async function POST(req) {
   });
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
-
+  console.log(stream);
   // Respond with the stream
   return new StreamingTextResponse(stream);
 }
