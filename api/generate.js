7e3fd0b3a252fd2d2ee79521aff20428c16974a9
James Israel <yongeazi143@gmail.com>
2023-10-28 01:31:34 +0100

    api now works, testing final

--- api/generate.js
+++ api/generate.js
@@ -1,18 +1,50 @@
-// app/api/completion/route.js
-
 import { OpenAI } from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
+import { kv } from "@vercel/kv";
+import { Ratelimit } from "@upstash/ratelimit";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-function buildPrompt(prompt) {
-  return prompt.split("\n").map((message) => ({
-    role: "system",
-    content: message,
-  }));
-}
+// IMPORTANT! Set the runtime to edge: https://vercel.com/docs/functions/edge-functions/edge-runtime
+export const runtime = "edge";
 
 export async function POST(req) {
+  // Check if the OPENAI_API_KEY is set, if not return 400
+  if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
+    return new Response(
+      "Missing OPENAI_API_KEY – make sure to add it to your .env file.",
+      {
+        status: 400,
+      }
+    );
+  }
+  if (
+    process.env.NODE_ENV != "development" &&
+    process.env.KV_REST_API_URL &&
+    process.env.KV_REST_API_TOKEN
+  ) {
+    const ip = req.headers.get("x-forwarded-for");
+    const ratelimit = new Ratelimit({
+      redis: kv,
+      limiter: Ratelimit.slidingWindow(50, "1 d"),
+    });
+
+    const { success, limit, reset, remaining } = await ratelimit.limit(
+      `novel_ratelimit_${ip}`
+    );
+
+    if (!success) {
+      return new Response("You have reached your request limit for the day.", {
+        status: 429,
+        headers: {
+          "X-RateLimit-Limit": limit.toString(),
+          "X-RateLimit-Remaining": remaining.toString(),
+          "X-RateLimit-Reset": reset.toString(),
+        },
+      });
+    }
+  }
+
   // Extract the `prompt` from the body of the request
   const { prompt } = await req.json();
 
